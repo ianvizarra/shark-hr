a2dfcc00310082c6f749f60851b5ccf9e9381b45
ianvizarra <ian.vizarra@gmail.com>
2020-05-11 01:29:52 +0800

    New components

---
+++ resources/js/components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="font-bold text-xl text-teal-700">
+      {{$page.app}}
+  </h1>
+</template>
---
+++ resources/js/components/MainMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ml-10 flex items-baseline">
+      <inertia-link :href="route('dashboard')" :class="isUrl('dashboard') ? 'menu-active' : 'menu-normal'">Dashboard</inertia-link>
+  </div>
+</template>
+
+<script>
+import Icon from '@components/Icon'
+
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    url: String,
+  },
+  methods: {
+    isUrl(...urls) {
+      if (urls[0] === '') {
+        return this.url === ''
+      }
+      return urls.filter(url => this.url.startsWith(url)).length
+    },
+  },
+}
+</script>
